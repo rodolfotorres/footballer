15e7fb0dc293500ab07918474aa000e78904fd87
Rodolfo Torres <rtorres@simplifydigital.co.uk>
2015-06-26 00:06:03 +0100

    added callback hell

--- src/app/league/league.controller.js
+++ src/app/league/league.controller.js
@@ -1,8 +1,13 @@
 'use strict';
 
-angular.module('footballer').controller('LeagueCtrl', function($scope, $stateParams, $filter, Teams, ngTableParams){
+angular.module('footballer').controller('LeagueCtrl', function($scope, $stateParams, $filter, Teams, leagueService){
   $scope.id = $stateParams.id;
   $scope.league;
+
+  leagueService.getLeague($stateParams.id, function(result){
+    console.log(result);
+  });
+
   Teams.get({id: $stateParams.id}, function(data){
     console.log(data);
     $scope.league = data;
--- src/app/services/football-service.js
+++ src/app/services/football-service.js
@@ -11,3 +11,11 @@ angular.module('footballer').factory('Teams', function($resource){
 angular.module('footballer').factory('Leagues', function($resource){
   return $resource('/assets/data/leagues.json');
 });
+
+angular.module('footballer').service('leagueService', function($resource){
+  this.getLeague = function(id, callback){
+    $resource('/assets/data/leagues.json').get(function(data){
+      callback(data.leagues[0]);
+    });
+  };
+});
